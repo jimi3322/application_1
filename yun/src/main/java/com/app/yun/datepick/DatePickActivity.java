8ad9e891acb59c12844d125c709c0317421ea7d2
jimi3322 <1468776156@qq.com>
2020-08-21 17:59:42 +0800

    时间插件改变样式

--- yun/src/main/java/com/app/yun/datepick/DatePickActivity.java
+++ yun/src/main/java/com/app/yun/datepick/DatePickActivity.java
@@ -5,10 +5,13 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,13 +19,14 @@ import com.app.yun.R;
 
 import java.util.Calendar;
 
-public class DatePickActivity extends AppCompatActivity implements View.OnClickListener, DatePicker.OnDateChangedListener, TimePicker.OnTimeChangedListener {
+public class DatePickActivity extends AppCompatActivity implements View.OnClickListener, DatePicker.OnDateChangedListener, TimePicker.OnTimeChangedListener ,CustomDatePickerDialogFragment.OnSelectedDateListener{
     private Context context;
     private LinearLayout llDate, llTime;
     private TextView tvDate, tvTime;
     private int year, month, day, hour, minute;
     //在TextView上显示的字符
     private StringBuffer date, time;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +37,10 @@ public class DatePickActivity extends AppCompatActivity implements View.OnClickL
         time = new StringBuffer();
         initView();
         initDateTime();
+
+        //改变样式的时间选择器
+        button = findViewById(R.id.datepicker);
+        button.setOnClickListener(this);
     }
 
     /**
@@ -46,6 +54,7 @@ public class DatePickActivity extends AppCompatActivity implements View.OnClickL
         tvTime =  findViewById(R.id.tv_time);
         llDate.setOnClickListener(this);
         llTime.setOnClickListener(this);
+
     }
 
     /**
@@ -85,8 +94,11 @@ public class DatePickActivity extends AppCompatActivity implements View.OnClickL
                 final AlertDialog dialog = builder.create();
                 View dialogView = View.inflate(context, R.layout.dialog_date, null);
                 final DatePicker datePicker = (DatePicker) dialogView.findViewById(R.id.datePicker);
-
-                dialog.setTitle("设置日期");
+                //如果只要日历部分，隐藏header
+                ViewGroup mContainer = (ViewGroup) datePicker.getChildAt(0);
+                View header = mContainer.getChildAt(0);
+                header.setVisibility(View.GONE);
+                // dialog.setTitle("设置日期");
                 dialog.setView(dialogView);
                 dialog.show();
                 //初始化日期监听事件
@@ -121,6 +133,11 @@ public class DatePickActivity extends AppCompatActivity implements View.OnClickL
                 dialog2.setView(dialogView2);
                 dialog2.show();
                 break;
+            case R.id.datepicker:
+                showDatePickDialog();
+                break;
+            default:
+                break;
         }
     }
 
@@ -151,5 +168,40 @@ public class DatePickActivity extends AppCompatActivity implements View.OnClickL
     public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
         this.hour = hourOfDay;
         this.minute = minute;
+    }
+
+    /**
+     * 改变样式的时间选择器
+     */
+    long dayf = 24 * 60 * 60 * 1000;
+    private void showDatePickDialog() {
+        CustomDatePickerDialogFragment fragment = new CustomDatePickerDialogFragment();
+        fragment.setOnSelectedDateListener(this);
+        Bundle bundle = new Bundle();
+        Calendar currentDate = Calendar.getInstance();
+        currentDate.setTimeInMillis(System.currentTimeMillis());
+        currentDate.set(Calendar.HOUR_OF_DAY,0);
+        currentDate.set(Calendar.MINUTE,0);
+        currentDate.set(Calendar.SECOND,0);
+        currentDate.set(Calendar.MILLISECOND,0);
+        bundle.putSerializable(CustomDatePickerDialogFragment.CURRENT_DATE,currentDate);
+
+
+        long start = currentDate.getTimeInMillis() - dayf * 2;
+        long end = currentDate.getTimeInMillis() - dayf;
+        Calendar startDate = Calendar.getInstance();
+        startDate.setTimeInMillis(start);
+        Calendar endDate = Calendar.getInstance();
+        endDate.setTimeInMillis(end);
+        bundle.putSerializable(CustomDatePickerDialogFragment.START_DATE,startDate);
+        bundle.putSerializable(CustomDatePickerDialogFragment.END_DATE,currentDate);
+
+        fragment.setArguments(bundle);
+        fragment.show(getSupportFragmentManager(),CustomDatePickerDialogFragment.class.getSimpleName());
+    }
+
+    @Override
+    public void onSelectedDate(int year, int monthOfYear, int dayOfMonth) {
+        Toast.makeText(DatePickActivity.this,year+"年"+(monthOfYear+1)+"月"+dayOfMonth+"日", Toast.LENGTH_SHORT).show();
     }
 }
